f22235707ca8ee95d96dbd26a6a1a18d629c7c92
Schrodingers-Cats <adityasen120@gmail.com>
2022-05-28 13:22:09 -0400

    Error Catching

--- Automated Internship Emailer.py
+++ Automated Internship Emailer.py
@@ -13,6 +13,7 @@ NAME1 = "Aditya Sen"
 NAME2 = "Aditya Bhatia"
 NAME3 = "Steven Rowe"
 NAME4 = "Sebastian Spernac"
+NAME5 = 'David "Dodo" Rowe'
 TEAM_NAME = "Dodowranglers"
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
@@ -39,7 +40,7 @@ Hello {subject_name},
 
 Cancer doesn't sleep, and neither will we. On June 3rd and 4th, we will run in relay for 12 consecutive hours in solidarity with cancer patients, survivors, and those we have lost.
 
-Please help us reach our team fundraising goal of $2000. No matter how much you donate, or how insignificant is seems to you, every dollar goes a long way. Money donated will go towards cancer research, furthering our efforts to win the battle with cancer.
+Please help us reach our team fundraising goal of $1500. No matter how much you donate, or how insignificant is seems to you, every dollar goes a long way. Money donated will go towards cancer research, furthering our efforts to win the battle with cancer.
 
 Together, we can do this. Help us to relay for life!
 
@@ -47,7 +48,7 @@ Donate at:
 https://support.cancer.ca/site/TR/RelayForLife/RFLY_NW_odd_?team_id=489955&pg=team&fr_id=28128
 
 ---
-{NAME1}, {NAME2}, {NAME3}, {NAME4}
+{NAME1}, {NAME2}, {NAME3}, {NAME4}, {NAME5}
 Oakville, ON
 Iroquois Ridge High School
 {TEAM_NAME}
@@ -57,20 +58,24 @@ Iroquois Ridge High School
     message = message.as_string()
     # Logs in and sends mail
     #NOTE after with statement STMP quits automatically 
-    with smtplib.SMTP("smtp-mail.outlook.com", 587) as server:
-        server.starttls() #starts TLS (security)
+    try:
+        with smtplib.SMTP("smtp-mail.outlook.com", 587) as server:
+            server.starttls() #starts TLS (security)
 
-        server.login(EMAIL, PASSWORD) #login to my google gmail
-        server.sendmail(EMAIL, subject_email_address, message) #send the message
+            server.login(EMAIL, PASSWORD) #login to my google gmail
+            server.sendmail(EMAIL, subject_email_address, message) #send the message
+    except:
+        return "Unsuccessful"
 
-    return "Successful"
+    finally:
+        return "Successful"
 
 
 #array to check if all emails when through
 email_check = []
 #get company name and emails
 #iterate through all the rows in the csv file
-job_csv = open("Untitled spreadsheet - Sheet1.csv")
+job_csv = open("Relay_for_life_-_Sheet2.csv")
 job_csv.readline()
 job_csv = csv.reader(job_csv)
 
